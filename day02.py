579c5a2e5ae9482925c798154399432c61d61469
normal <specialnormal@naver.com>
2024-01-15 15:42:44 +0900

    v12 python day02

--- day02.py
+++ day02.py
@@ -1,3 +1,10 @@
+# temp = [0]
+# if temp:
+#     print("원소가 존재")
+# else :
+#     print("비어있는 리스트")
+
+
 menu = input("1) Fahrenheit -> Celsius  2) Celsius -> Fahrenheit  3) Quit program : ")
 
 if menu == '1' :
@@ -7,6 +14,7 @@ if menu == '1' :
 elif menu == '2' :
     celsius = float(input("섭씨온도 입력 : "))
     print(f'fahrenheit : {(celsius*(9/5)+32):.4f}F, Celsius : {celsius}')
-
+else :
+    print('Terminate Program.')
 # print(int('1A', 16))
 
