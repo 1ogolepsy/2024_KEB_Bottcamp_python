08a2c9d0123cf40f8ec70fdb92fc6a06ed42f699
normal <specialnormal@naver.com>
2024-01-15 15:55:54 +0900

    homework day02: infinity loop F to C

--- day02.py
+++ day02.py
@@ -1,8 +1,16 @@
-letter = input("Input alphabet letter : ")
-# vowels = {"a",'e','i','o','u'}
-vowels = 'aeiuo'
-print(type(vowels))
-if letter in vowels :
-    print(f'{letter} is a vowel~')
-else:
-    print(f'{letter} is a consonant!')
+while True :
+    menu = input("1) Fahrenheit -> Celsius  2) Celsius -> Fahrenheit  3) Quit program : ")
+
+    if menu == '1' :
+
+        fahrenheit = float(input("화씨온도 입력 : "))
+        print(f'fahrenheit : {fahrenheit}F, Celsius : {(fahrenheit - 32)*(5/9):.2f}')
+    elif menu == '2' :
+        celsius = float(input("섭씨온도 입력 : "))
+        print(f'fahrenheit : {(celsius*(9/5)+32):.4f}F, Celsius : {celsius}')
+    else :
+        print('Terminate Program.')
+        break
+
+
+
