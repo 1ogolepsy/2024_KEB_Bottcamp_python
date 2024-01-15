ff485743d55d9673b353b5c8692c63d48bd20cb2
normal <specialnormal@naver.com>
2024-01-15 14:56:56 +0900

    v11 python day02

--- day02.py
+++ day02.py
@@ -1,5 +1,12 @@
-fahrenheit = float(input("화씨온도 입력 : "))
-print(f'fahrenheit : {fahrenheit}F, Celsius : {(fahrenheit - 32)*(5/9):.2f}')
+menu = input("1) Fahrenheit -> Celsius  2) Celsius -> Fahrenheit  3) Quit program : ")
+
+if menu == '1' :
+
+    fahrenheit = float(input("화씨온도 입력 : "))
+    print(f'fahrenheit : {fahrenheit}F, Celsius : {(fahrenheit - 32)*(5/9):.2f}')
+elif menu == '2' :
+    celsius = float(input("섭씨온도 입력 : "))
+    print(f'fahrenheit : {(celsius*(9/5)+32):.4f}F, Celsius : {celsius}')
 
 # print(int('1A', 16))
 
