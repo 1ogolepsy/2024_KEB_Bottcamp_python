a0e721bc4094829a8db098b822180f92cfc3fe7c
normal <specialnormal@naver.com>
2024-01-15 13:21:39 +0900

    v07 python day02

--- day02.py
+++ day02.py
@@ -6,6 +6,11 @@
 # print(cash)
 # print(type(cash))
 
-base_number = int(input('INput base number'))
+base_number = int(input('Input base number :'))
 exponent_number = int(input('Input exponent number'))
-print(f'밑 {base_number}, 지수는 {exponent_number}, 결과값은 {base_number**exponent_number}')
+# print(f'밑 {base_number}, 지수는 {exponent_number}, 결과값은 {base_number**exponent_number}')
+# print(f'밑 {base_number}, 지수는 {exponent_number}, 결과값은 {pow(base_number,exponent_number)}')
+# print('밑 {0}, 지수는 {1}, 결과값은 {2}'.format(base_number, exponent_number, pow(base_number,exponent_number)))
+
+#c like
+print('밑은 %d, 지수는 %d, 결과 값은 %d' % (base_number, exponent_number, pow(base_number,exponent_number)))
