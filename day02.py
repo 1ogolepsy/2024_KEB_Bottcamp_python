bd7926e1e189fe9ca86418e87240f41ff874c456
normal <specialnormal@naver.com>
2024-01-15 15:48:04 +0900

    v13 python day02

--- day02.py
+++ day02.py
@@ -1,20 +1,8 @@
-# temp = [0]
-# if temp:
-#     print("원소가 존재")
-# else :
-#     print("비어있는 리스트")
-
-
-menu = input("1) Fahrenheit -> Celsius  2) Celsius -> Fahrenheit  3) Quit program : ")
-
-if menu == '1' :
-
-    fahrenheit = float(input("화씨온도 입력 : "))
-    print(f'fahrenheit : {fahrenheit}F, Celsius : {(fahrenheit - 32)*(5/9):.2f}')
-elif menu == '2' :
-    celsius = float(input("섭씨온도 입력 : "))
-    print(f'fahrenheit : {(celsius*(9/5)+32):.4f}F, Celsius : {celsius}')
-else :
-    print('Terminate Program.')
-# print(int('1A', 16))
-
+letter = input("Input alphabet letter : ")
+# vowels = {"a",'e','i','o','u'}
+vowels = 'aeiuo'
+print(type(vowels))
+if letter in vowels :
+    print(f'{letter} is a vowel~')
+else:
+    print(f'{letter} is a consonant!')
