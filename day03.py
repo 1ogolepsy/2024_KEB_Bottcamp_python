8246963a100339e26cccc5c008d073e2f6c05df3
normal <specialnormal@naver.com>
2024-01-16 14:37:16 +0900

    v22 assignment day03

--- day03.py
+++ day03.py
@@ -2,17 +2,17 @@
 # print("{0[python]} {0[database]}".format(subject))
 
 number = int(input('Input number : '))
-cnt = 0
+is_prime = True
 i = 2
 
 while i < number:
     if number % i == 0:
-        cnt += 1
+        is_prime = False
         break
         # print(i)
     i += 1
 
-if cnt == 0:
+if is_prime:
     print(f'{number} is prime number')
 else:
     print(f'{number} is NOT prime number')
