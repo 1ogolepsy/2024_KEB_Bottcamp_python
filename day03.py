4c72f07590f1956f429caa477ce60b683d5b85ad
normal <specialnormal@naver.com>
2024-01-16 11:39:49 +0900

    v19 assignment day03

--- day03.py
+++ day03.py
@@ -1,19 +1,7 @@
-course = "* KEB 2024#.!*# KEB! a Bootcamp KEB...*!#"
-# print(course)
-# print(course.replace('KEB','Inha'))
-# course = course.replace('KEB', 'Inha')
-# print(course)
-
-# print(course)
-# course = course.replace('KEB', "Inha", 2)
-# print(course)
-# print(course.strip('!#.*'))
-
-print(course.find('KEB'))
-print(course.rfind('KEB'))
-print(course.index('KEB'))
-print(course.rindex('KEB'))
-
-print(course.find('Inha'))
-print(course.index('Inha'))
+subjects = "python c++ database linux"
+subject = input('수강신청과목 입력 : ')
+if subjects.find(subject) != -1:
+    print(f'해당 과목이 존재합니다. 위치는{subjects.find(subject)}번 인덱스입니다.')
+else:
+    print('해당과목이 존재하지 않습니다.')
 
