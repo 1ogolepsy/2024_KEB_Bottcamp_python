5ef17e79108ad174a6dfa96f7128cf8ac02db438
normal <specialnormal@naver.com>
2024-01-16 13:37:11 +0900

    v20 assignment day03

--- day03.py
+++ day03.py
@@ -1,7 +1,8 @@
-subjects = "python c++ database linux"
+subjects = "pythoncdatabaselinux"
+print(subjects.isalnum())
 subject = input('수강신청과목 입력 : ')
-if subjects.find(subject) != -1:
-    print(f'해당 과목이 존재합니다. 위치는{subjects.find(subject)}번 인덱스입니다.')
-else:
-    print('해당과목이 존재하지 않습니다.')
 
+try:
+    print(f'해당 과목이 존재합니다. 위치는{subjects.index(subject)}번 인덱스입니다.')
+except ValueError:
+    print('해당과목이 존재하지 않습니다.')
