c532499b87691a677529d88d959891e57332f813
normal <specialnormal@naver.com>
2024-01-16 15:50:32 +0900

    v28 final day03

--- day03.py
+++ day03.py
@@ -1,15 +1,24 @@
-univ = 'inha'
-i = 0
-while i < len(univ):
-    print(univ[i], end=' ')
-    i += 1
+# subject = {'python' : 'ken', 'c++':'sung','data structure':'kim','database':'kang'}
+# print("{0[python]} {0[database]}".format(subject))
 
-print()
+numbers = input('First number Second number: ').split()
+n1 = int(numbers[0])
+n2 = int(numbers[1])+1
 
-for letter in univ:
-    print(letter, end=' ')
+if n2 < n1:
+    n1, n2 = n2, n1
 
-print()
+for number in range(n1, n2):
+    if number < 2:
+        continue
 
-for k in range(0, len(univ), 1):
-    print(univ[k], end=' ')
+    is_prime = True
+
+    for i in range(2,number):
+
+        if number % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(number)
+
