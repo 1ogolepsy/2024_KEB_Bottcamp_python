963d0021d70bd776991c962c1a7e6670bb059702
normal <specialnormal@naver.com>
2024-01-16 09:23:01 +0900

    v15 assignment day03

--- day02.py
+++ day02.py
@@ -1,16 +1,13 @@
-while True :
+while True:
     menu = input("1) Fahrenheit -> Celsius  2) Celsius -> Fahrenheit  3) Quit program : ")
 
-    if menu == '1' :
+    if menu == '1':
 
         fahrenheit = float(input("화씨온도 입력 : "))
-        print(f'fahrenheit : {fahrenheit}F, Celsius : {(fahrenheit - 32)*(5/9):.2f}')
-    elif menu == '2' :
+        print(f'fahrenheit : {fahrenheit}F, Celsius : {(fahrenheit - 32)*(5/9):.4f}')
+    elif menu == '2':
         celsius = float(input("섭씨온도 입력 : "))
         print(f'fahrenheit : {(celsius*(9/5)+32):.4f}F, Celsius : {celsius}')
-    else :
+    elif menu == '3':
         print('Terminate Program.')
         break
-
-
-
