3f5463f0047aa86508cd1f1a1a0b1b7b3c9866ea
normal <specialnormal@naver.com>
2024-01-16 14:40:43 +0900

    v26 assignmnet day03

--- day03.py
+++ day03.py
@@ -3,16 +3,20 @@
 
 number = int(input('Input number : '))
 is_prime = True
-i = 2
 
-while i < number:
-    if number % i == 0:
-        is_prime = False
-        break
-        # print(i)
-    i += 1
-
-if is_prime:
-    print(f'{number} is prime number')
-else:
+if number <= 2:
     print(f'{number} is NOT prime number')
+else:
+    i = 2
+
+    while i < number:
+        if number % i == 0:
+            is_prime = False
+            break
+            # print(i)
+        i += 1
+
+    if is_prime:
+        print(f'{number} is prime number')
+    else:
+        print(f'{number} is NOT prime number')
