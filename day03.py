5ba6c02f88fa9e20d29fefa032b59e363ce70098
normal <specialnormal@naver.com>
2024-01-16 14:13:08 +0900

    v21 assignment day03

--- day03.py
+++ day03.py
@@ -1,8 +1,18 @@
-subjects = "pythoncdatabaselinux"
-print(subjects.isalnum())
-subject = input('수강신청과목 입력 : ')
+# subject = {'python' : 'ken', 'c++':'sung','data structure':'kim','database':'kang'}
+# print("{0[python]} {0[database]}".format(subject))
 
-try:
-    print(f'해당 과목이 존재합니다. 위치는{subjects.index(subject)}번 인덱스입니다.')
-except ValueError:
-    print('해당과목이 존재하지 않습니다.')
+number = int(input('Input number : '))
+cnt = 0
+i = 2
+
+while i < number:
+    if number % i == 0:
+        cnt += 1
+        break
+        # print(i)
+    i += 1
+
+if cnt == 0:
+    print(f'{number} is prime number')
+else:
+    print(f'{number} is NOT prime number')
