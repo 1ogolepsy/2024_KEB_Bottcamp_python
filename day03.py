2e7b6c1ac32f1af16ca5a9b6b67721b7bf14a87f
normal <specialnormal@naver.com>
2024-01-16 10:34:59 +0900

    v16 assignment  day 03

---
+++ day03.py
@@ -0,0 +1,9 @@
+uiversity = "Inha\nUniversity!"
+print(uiversity[0:4])
+print(uiversity[:-11])
+
+number1 = input("First number : ")
+number2 = input("Second number : ")
+print((number1 + number2))
+print(number1 * 3)
+# print(number1 + 3) #type error
