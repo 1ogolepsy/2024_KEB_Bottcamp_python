eea3e469062d9f4e11dfecacebfb60732415eab4
normal <specialnormal@naver.com>
2024-01-17 09:46:18 +0900

    v3.3 day04

--- day04.py
+++ day04.py
@@ -1,3 +1,6 @@
+#성능개선 > 1. 4번에서 입력값이 숫자가 하나일 때 등 사용법 안내, 2. 반복되는 것들 함수 3. 화씨가 절대온도 밑으로 내려가는 이슈
+#
+
 while True :
     menu = input("1) Fahrenheit -> Celsius  2) Celsius -> Fahrenheit 3) judge prime number \
 4) get prime number in range 5) Quit program : ")
@@ -32,24 +35,28 @@ while True :
                 print(f'{number} is NOT prime number')
 
     elif menu == '4':
+        #여기서부터
         numbers = input('First number Second number: ').split()
         n1 = int(numbers[0])
         n2 = int(numbers[1]) + 1
 
         if n2 < n1:
             n1, n2 = n2, n1
-
+        #여기까지 간소화 가능
         for number in range(n1, n2):
             if number < 2:
                 continue
 
             is_prime = True
 
-            for i in range(2, number):
+            i = 2
+            while i*i < number:
 
                 if number % i == 0:
                     is_prime = False
                     break
+                i += 1
+
             if is_prime:
                 print(number)
 
