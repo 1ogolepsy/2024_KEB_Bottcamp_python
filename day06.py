1ff76100487ce7bba445e6ef0eba9b558772bf48
normal <specialnormal@naver.com>
2024-01-19 15:04:10 +0900

    v5.6 donkey day06

--- day06.py
+++ day06.py
@@ -1,34 +1,23 @@
-class Pokemon:
-    def __init__(self, name):
-        self.name = name
+class Animal:
+    def says(self):
+        return 'I speak!'
 
-    def attack(self, target):
-        print(f'{self.name}이(가) {target.name}을(를) 공격!')
+class horse(Animal):
+    def says(self):
+        return 'Neigh!'
 
+class Donkey(Animal):
+    def says(self):
+        return 'hee-hae'
 
-class Pikachu(Pokemon):
-    def __init__(self, name, type):
-        super().__init__(name)
-        self.type = type
-    def attack(self, target):
-        print(f'{self.name}이(가) {target.name}을(를) {type} 공격!')
+class Mule(Donkey, horse):
+    def says(self):
+        return '노새 노새 젊어서 노새'
+class hinny(horse, Donkey):
+    def says(self):
+        return '버새 버새'
 
-    def electric_info(self):
-        print('전기 계열의 공격을 합니다.')
-
-class Squirtle(Pokemon):
-    pass
-
-class Agumon:
-    pass
-
-p1 = Pikachu('피카추', '전기')
-p2 = Squirtle('꼬부기')
-p3 = Pokemon('아무개')
-p1.electric_info()
-
-p1.attack(p2)
-print(p1.type)
-p2.attack(p1)
-print(p1.name)
-print(issubclass(Pikachu, Pokemon))
+mi = Donkey()
+h1 = hinny()
+print(Mule.mro())
+print(mi.says())
