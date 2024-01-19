8be2f8d3b38f93d244df43f84d39f0b8757d3ee0
normal <specialnormal@naver.com>
2024-01-19 15:31:43 +0900

    v5.7 day06

--- day06.py
+++ day06.py
@@ -1,23 +1,39 @@
-class Animal:
-    def says(self):
-        return 'I speak!'
+class FlyingMixin:
+    def fly(self):
+        return f'{self.name}이(가) 하늘을 훨훨 날아갑니다~'
 
-class horse(Animal):
-    def says(self):
-        return 'Neigh!'
+class SwimmingMixin:
+    def swim(self):
+        return f'{self.name}이(가) 수영을 합니다.'
 
-class Donkey(Animal):
-    def says(self):
-        return 'hee-hae'
 
-class Mule(Donkey, horse):
-    def says(self):
-        return '노새 노새 젊어서 노새'
-class hinny(horse, Donkey):
-    def says(self):
-        return '버새 버새'
+class Pokemon:
+    def __init__(self, name):
+        self.name = name
+    def attack(self):
+        print('attack')
+    @property
+    def name(self):
+        print('inside getter')
+        return self.name
+    @name.setter
+    def name(self, new_name):
+        print('inside setter')
+        self.name = new_name
 
-mi = Donkey()
-h1 = hinny()
-print(Mule.mro())
-print(mi.says())
+
+class Charizard(Pokemon, FlyingMixin):
+    pass
+
+class Gyarados(Pokemon, SwimmingMixin):
+    pass
+
+g1 = Gyarados('갸라도스')
+c1 = Charizard('리자몽')
+
+print(g1.swim())
+print(c1.fly())
+Charizard.attack(c1)
+print(g1.get_name())
+g1.set_name('잉어킹')
+print(g1.get_name())
