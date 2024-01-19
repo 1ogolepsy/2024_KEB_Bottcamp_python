6839d75bcd89a62a31d3decb156651025f266463
normal <specialnormal@naver.com>
2024-01-19 15:49:56 +0900

    v5.8 end of day06

--- day06.py
+++ day06.py
@@ -1,25 +1,25 @@
 class FlyingMixin:
     def fly(self):
-        return f'{self.name}이(가) 하늘을 훨훨 날아갑니다~'
+        return f'{self.__name}이(가) 하늘을 훨훨 날아갑니다~'
 
 class SwimmingMixin:
     def swim(self):
-        return f'{self.name}이(가) 수영을 합니다.'
+        return f'{self.__name}이(가) 수영을 합니다.'
 
 
 class Pokemon:
     def __init__(self, name):
-        self.name = name
+        self.__name = name
     def attack(self):
         print('attack')
     @property
     def name(self):
         print('inside getter')
-        return self.name
+        return self.__name
     @name.setter
     def name(self, new_name):
         print('inside setter')
-        self.name = new_name
+        self.__name = new_name
 
 
 class Charizard(Pokemon, FlyingMixin):
@@ -30,10 +30,11 @@ class Gyarados(Pokemon, SwimmingMixin):
 
 g1 = Gyarados('갸라도스')
 c1 = Charizard('리자몽')
-
-print(g1.swim())
-print(c1.fly())
-Charizard.attack(c1)
-print(g1.get_name())
-g1.set_name('잉어킹')
-print(g1.get_name())
+#
+# print(g1.swim())
+# print(c1.fly())
+# Charizard.attack(c1)
+# print(g1.get_name())
+# g1.set_name('잉어킹')
+# print(g1.get_name())
+print(g1._Pokemon__name)
