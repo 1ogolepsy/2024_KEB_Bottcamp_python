7bcf0276368e26dca4b4623313838e21ceefa6dc
normal <specialnormal@naver.com>
2024-01-22 10:22:11 +0900

    python v6.9 day07

--- test.py
+++ test.py
@@ -5,14 +5,4 @@ def change(keep):
     keep =False
     return keep
 
-keep =True
-i = 0
-while keep:
-    if i ==5:
-        change(keep)
-        print('keep')
-    else:
-        print(i)
-        if i == 10:
-            break
-    i += 1
+print(3    +   9)
